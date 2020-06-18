5ecc42efc21bf9a91a483b85878e71185f29fc37
xuyongchao <xuyongchao6585@ipalfish.com>
2020-06-18 19:27:37 +0800

    feat: build

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -5,11 +5,12 @@ var path = require('path')
 var WebpackConsoleClear = require('../index').WebpackConsoleClear
 
 module.exports = {
+  mode: 'production',
   cache: true,
   context: __dirname,
   entry: './main.js',
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, '../dist'),
     filename: '[hash].main.js'
   },
   plugins: [
